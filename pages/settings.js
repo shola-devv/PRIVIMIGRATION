8182d2733b9cf76e213402761ae0b40aea267cb8
shola-devv <creekemmanuelf@gmail.com>
2024-09-30 01:19:44 -0700

    my commit

---
+++ pages/settings.js
@@ -0,0 +1,31 @@
+
+import Image from "next/image"
+  
+   export default settings = ()=>{
+
+return(
+
+<>
+<h1>
+
+</h1>
+
+<div>
+<ol>
+    <li>
+      profile settings
+    </li>
+    <li>
+       general settings
+    </li>
+    <li>
+        dark mode
+    </li>
+</ol>
+
+</div>
+</>
+
+)
+
+  }
