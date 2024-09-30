9628e279ceb710df7c4fedb428225a658e39c76e
shola-devv <creekemmanuelf@gmail.com>
2024-09-30 01:26:48 -0700

    my commit for this night

---
+++ components/Notifications.js
@@ -0,0 +1,37 @@
+
+
+ export default Notification = ()=>{
+
+//theres something like a map function from the fetched function -using axios-
+const fetcher = async()=>{
+ // fetcher
+    const data = await /* axios.*/ fetch();
+
+
+
+
+}
+return (
+
+<>
+
+<div>
+<ul>
+
+    <li>
+
+    </li>
+    <li>
+        
+        </li>
+        <li>
+        
+        </li>
+</ul>
+
+</div>
+</>
+
+)
+
+}
