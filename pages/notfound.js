4f0a09b125a8368479f789f9f174530c1db00931
unknown <creekemmanul.com>
2024-09-30 01:01:18 -0700

    my own commit for a nice night, no tire me walai

--- pages/notfound.js
+++ pages/notfound.js
@@ -1,1 +1,16 @@
+import Link from "next/link"
 
+
+
+export default notFound = ()=>{
+
+return(
+<>
+
+<h1> PAGE NOT FOUND</h1>
+<h2><Link href='/' className='text-blue-500'>go back home</Link></h2>
+</>
+
+)
+
+}
