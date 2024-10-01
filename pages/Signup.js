6e763cfd820d0a989b79d568cd0115a2b4b70c54
shola-devv <creekemmanuelf@gmail.com>
2024-10-01 13:47:15 -0700

    main nighty night

--- pages/Signup.js
+++ pages/Signup.js
@@ -4,25 +4,24 @@ import Navbar from '../components/navbar.js';
 import Link from 'next/link';
 
 // <Navbar />
-export default function  Signup(){
-
+export default function  Login(){
+ 
  return(
         
-        <div className={styles.body}>
+        <div className='{styles.body} overflow-hidden'>
            
-        <div>
-          <h1 className='mx-auto my-auto  sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-3xl  '>     PRIVIMIGRATION</h1>
-          <h2 className='sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-3xl'>SIGNUP</h2>
+        <div className='bg-slate-200 rounded-xl p-20 sm:p-6 md:p-10 xl:20px'>
+          <h1 className='mx-auto my-auto  sm:text-lg md:text-xl lg:text-2xl p-8 xl:text-3xl 2xl:text-3xl  font-bold'>     PRIVIMIGRATION</h1>
+          <h2 className='sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-3xl font-semibold'>SignUp</h2>
          <form>
          <label>Username:</label>
-          <input type='text' className='border-4 border-slate-400 rounded-lg w-80 h-12' ></input><br></br>
-          <label>password</label>
-          <input type='password' className='border-4 border-slate-400 rounded-lg w-80 h-12 m-8' ></input><br></br>
-          <input type ='submit' className=' border-4 border-blue-400 cursor-pointer bg-blue-200 rounded-md h-12 w-32 m-6'></input>
+        <input type='text' className='border-4 border-slate-400 rounded-lg w-80 h-12' ></input><br></br>
+          <label>password:</label>
+          <input type='password' className='border-4 border-slate-400 rounded-lg w-80 h-12 m-8 mx--16' ></input> <br></br>
+          <input type ='submit' value='       LOGIN' className=' border-4 border-blue-400 cursor-pointer bg-blue-200 rounded-md h-12 w-32 m-6'></input>
 
          </form>
-         <h1> already have an account? <Link className='text-blue-700'href='/Login'>login</Link></h1>
+         <h1> already have an account ? <Link className='text-blue-700'href='/Login'>Login</Link></h1>
         </div>
-        </div>
-    )
-}
+       </div>
+        )}
