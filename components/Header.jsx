e30b1ad210eb9362e3dd3e96aefa58b3550d97e5
shola-devv <creekemmanuelf@gmail.com>
2024-10-04 05:24:22 -0700

    my latest message

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link.js'
 
 const Header = ()=>{
     let title = <h1 className='text-xl  md:text-2xl font-bold text-blue-400 font-mono'>privimigration</h1>
+    
     return( 
         <div className='flex flex-col md:flex-row justify-between items-center py-2 px-6 md:px-6 bg-white shadow-blue-300 shadow-md rounded-lg'>
         <div className='flex items-center mb-4 md:mb-0'>
@@ -12,12 +13,7 @@ const Header = ()=>{
         <div className='flex items-center '>
             
 
-            <Image src="../worldGlobe.svg"
-                    alt="Vercel Logo"
-                    width={40}
-                    height={50}
-                    priority 
-                     />
+            
                     <div className='ml-3 md:ml- 20'> {title}</div>
                    
                     </div>
